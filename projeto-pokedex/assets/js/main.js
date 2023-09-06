4ced07f88a5f1912fc33031a1d49a0bd5c6a2658
thalesacardoso <thales.dev@hotmail.com>
2023-09-06 16:25:38 -0300

    Consumindo API parte 1

--- projeto-pokedex/assets/js/main.js
+++ projeto-pokedex/assets/js/main.js
@@ -1,17 +1,36 @@
-console.log("teste");
-
-// Indica a partir de qual ordem vai chamar os elementos
 const offset = 0;
-
-// Limita a quantidade de dados por página
-const limit = 5;
-
-// URL de onde estará puxando as informações
+const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
-// A Função fetch() é responsável por fazer a Requisição dos dados externos( API )
-// A Função then() é responsável por mostrar o que veio de resposta.
-fetch(url).then(function (response) {
-   // Mostra o que veio como resposta da requisição
-   console.log(response);
-});
+
+function convertePokemonEmHTML(pokemon) {
+   return `
+            <div class="pokemon">
+					<span class="numero">#001</span>
+					<span class="titulo">${pokemon.name}</span>
+					<div class="box">
+						<ul class="pokemon-textos">
+							<li class="tipo">Grass</li>
+							<li class="tipo">Poison</li>
+						</ul>
+						<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name} ">
+					</div>
+				</div>`
+}
+
+let cards = document.getElementById('cards-pokemons')
+
+// Faz a requisição
+fetch(url)
+   // criação de funções com Arrow Function
+   .then((response) => response.json())         // recebe os dados pelo parametro response e converte em json.
+   .then((jsonBody) => jsonBody.results)        // recebe os dados em json pelo parametro jsonBody e depois acessa o atributo results.
+   .then((pokemonList) => {
+
+      for (let i = 0; i < pokemonList.length; i++) {
+         let poke = pokemonList[i];
+         cards.innerHTML += convertePokemonEmHTML(poke)
+      }
+
+   })
+   .catch((error) => console.error(error))
